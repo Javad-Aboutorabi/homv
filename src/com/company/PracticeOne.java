24524d9fe879ef675ab522d65a2488591ef9f81d
hossein <hossein.movaghari@gmail.com>
2022-04-21 02:32:36 +0430

    commit hossein2

--- src/com/company/PracticeOne.java
+++ src/com/company/PracticeOne.java
@@ -1,7 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PracticeOne {
     public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j <= 10; j++) {
+                System.out.print(j+"*"+i+"="+i*j+"\t");
 
+            }
+            System.out.println("khalaghiat");
+        }
+        }
     }
-}
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("lotfan tool ra varde konid: ");
+//        double x= input.nextDouble();
+//        Scanner input2= new Scanner(System.in);
+//        System.out.print("lotfan arz ra vared konid : ");
+//        double y= input.nextDouble();
+//        double  result = x*y/2 ;
+//        System.out.println("mashat mostatil shoma  "+result+"  mibashad.");
+//    }
---
+++ src/masahat.java
@@ -0,0 +1,15 @@
+import java.util.* ;
+public class masahat {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        System.out.println("lotfan tool ra varde konid:");
+
+
+
+
+    }
+
+
+
+
+}
