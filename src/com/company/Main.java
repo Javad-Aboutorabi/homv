4ec2e268c09db1eef7ffd43b3cd54d1154ac964c
javad <javad.aboutorabi@gmail.com>
2022-04-20 04:54:54 +0430

    add hello world to project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello world");
         int test = 0;
         double testDouble = 0.0;
         int a = 2;
