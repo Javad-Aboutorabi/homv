007ef47210cff1ddf32ade87e1f4ef117813d152
hossein <hossein.movaghari@gmail.com>
2022-05-08 03:40:45 +0430

    contact2

--- src/com/company/contact2.java
+++ src/com/company/contact2.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 public class contact2 {
     public static void main(String[] args) {
         ArrayList<contact> contacts = new ArrayList<>() ;
-       contact hossein = new contact("hossein","movaghari",2020,170000000,"hm@gmail.com","gorgan molaghati","single") ;
-       contact reza =new contact("reza","siahbalei",2345,171171717,"rs@gmail.com","gorgan ojaben     ","in rel");
-       contact mamad = new contact("mamad","zakeri     ",1475,17465446,"mz@gmail.com","gorgan sayyad","    married");
+       contact hossein = new contact("hossein","movaghari",911111111,170000000,"hm@gmail.com","gorgan molaghati","single") ;
+       contact reza =new contact("reza","siahbalei",911222222,171171717,"rs@gmail.com","gorgan ojaben     ","in rel");
+       contact mamad = new contact("mamad","zakeri     ",911333333,174654463,"mz@gmail.com","gorgan sayyad","    married");
        contacts.add(hossein);
        contacts.add(reza);
        contacts.add(mamad);
-        System.out.println("name:"+"\t \t"+"family:"+"\t\t"+"mobile number:"+"\t"+"home number:"+"\t \t"+"email:"+"\t \t "+"   address:"+"\t \t"+"        relationship:" ) ;
+        System.out.println("name:"+"\t \t"+"family:"+"\t\t "+"mobile number:"+"\t \t"+"home number:"+"\t \t"+"email:"+"\t \t "+"   address:"+"\t \t"+"        relationship:" ) ;
         for(int i=0 ; i<contacts.size() ; i++ ){
             System.out.println(contacts.get(i).getName()+"\t \t"+ contacts.get(i).getFamily()+"\t \t"+ contacts.get(i).getMobilenumber()+"\t \t"+ contacts.get(i).getHomenumber()+"\t \t "+ contacts.get(i).getEmail()+"\t \t"+ contacts.get(i).getAddress()+"\t \t"+contacts.get(i).getRelationship()+"\t \t") ;
         }
